75f212decd9ae438ba7a10cce651c194f7ee69be
Thiago Oliveira <thiago@signaes.com>
2025-01-16 10:11:02 -0300

    Refactor install script and remove zellij

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 brew update
 brew upgrade
 brew install --cask ghostty
-brew install fzf tmux tmuxinator the_silver_searcher ripgrep pyenv neovim rbenv dua-cli bottom macmon csvlens
+brew install fzf tmux tmuxinator the_silver_searcher ripgrep pyenv neovim rbenv dua-cli bottom macmon csvlens cmake wget lazygit
+brew tap mesca/luarocks
+brew install luarocks
 
 curl -o ~/.git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 brew install zsh-completion
 
-[[ $(ls ~/.vim | grep backup 2> /dev/null) == "" ]] && echo 'making ~/.vim/backup and ~/.vim/swap dirs' && \
-  mkdir ~/.vim/backup ~/.vim/swap 2> /dev/null
+[[ ! -d ~/.vim/backup ]] && mkdir ~/.vim/backup
+[[ ! -d ~/.vim/swap ]] && mkdir ~/.vim/swap
 
 echo "Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -17,14 +19,5 @@ echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
 
-brew install cmake
-brew install wget
-brew tap mesca/luarocks
-brew install luarocks
-brew install lazygit
-
 echo "Installing Deno"
 curl -fsSL https://deno.land/install.sh | sh
-
-echo "Installing Zellij"
-cargo install --locked zellij
