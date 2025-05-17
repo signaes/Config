d89814b7c32283376a213e4a918fa6e700404e69
Thiago Oliveira <thiago@signaes.com>
2025-05-17 12:34:07 -0300

    Change gen.lua config

--- dotfiles/nvim/lua/main/plugins/gen.lua
+++ dotfiles/nvim/lua/main/plugins/gen.lua
@@ -1,18 +1,18 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "phi4",         -- The default model to use.
-    quit_map = "q",         -- set keymap to close the response window
-    retry_map = "<c-r>",    -- set keymap to re-send the current prompt
-    accept_map = "<c-cr>",  -- set keymap to replace the previous selection with the last result
-    host = "localhost",     -- The host running the Ollama service.
-    port = "11434",         -- The port on which the Ollama service is listening.
-    display_mode = "split", -- The display mode. Can be "float" or "split" or "horizontal-split".
-    show_prompt = false,    -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-    show_model = true,      -- Displays which model you are using at the beginning of your chat session.
-    no_auto_close = false,  -- Never closes the window automatically.
-    file = false,           -- Write the payload to a temporary file to keep the command short.
-    hidden = false,         -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+    model = "phi4",                    -- The default model to use.
+    quit_map = "q",                    -- set keymap to close the response window
+    retry_map = "<c-r>",               -- set keymap to re-send the current prompt
+    accept_map = "<c-cr>",             -- set keymap to replace the previous selection with the last result
+    host = "localhost",                -- The host running the Ollama service.
+    port = "11434",                    -- The port on which the Ollama service is listening.
+    display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split".
+    show_prompt = false,               -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+    show_model = true,                 -- Displays which model you are using at the beginning of your chat session.
+    no_auto_close = true,              -- Never closes the window automatically.
+    file = false,                      -- Write the payload to a temporary file to keep the command short.
+    hidden = false,                    -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
     init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
     -- Function to initialize Ollama
     command = function(options)
