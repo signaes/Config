6ea4680cadbe9dc4bcd2dd2d90b6c9fbb7693e75
Thiago Oliveira <thiago@signaes.com>
2026-05-23 19:01:16 -0300

    Fix

--- dotfiles/nvim/lua/main/plugins/lsp.lua
+++ dotfiles/nvim/lua/main/plugins/lsp.lua
@@ -407,7 +407,7 @@ return {
 
       callback = function(event2)
         vim.lsp.buf.clear_references()
-        vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+        pcall(vim.api.nvim_clear_autocmds, { group = "lsp-highlight", buffer = event2.buf })
       end,
     })
 
